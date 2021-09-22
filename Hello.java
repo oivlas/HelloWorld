6ea4e717bce30f43e79b0970b8ac61dfc6a87337
Oivlas <71229362+oivlas@users.noreply.github.com>
2021-09-22 11:09:04 -0300

    Atualização turma 2TDSA Arquivo Hello.java
    Para essa nova versão, atualizamos o controle de repetição para 11  execuções e mudamos o texto atualizando o nome da turma.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=5;i++){
-         System.out.println("Hello World 2TDSF Top GitHub "+i);
+      for(int i=1;i<=11;i++){
+         System.out.println("Hello World turma 2TDSA sempre juntos! "+i);
       }
    }
 }
