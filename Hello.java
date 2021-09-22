90eae89e86c33a672f0e37bee22071af2c8799a8
Oivlas <71229362+oivlas@users.noreply.github.com>
2021-09-22 11:15:39 -0300

    Atualização 2TDSA no arquivo Hello.java
    Nessa  nova versão temos um controle de repetição menor (7 exibições) e um novo texto, agora na língua Portuguesa (Brasil).

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=11;i++){
-         System.out.println("Hello World turma 2TDSA sempre juntos! "+i);
+      for(int i=1;i<=7;i++){
+         System.out.println("Olá seres do planeta Terra, turma 2TDSA sempre juntos! "+i);
       }
    }
 }
