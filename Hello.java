37f3d8c0272fb4c8e4a09485fe86dd769194cfaf
Oivlas <71229362+oivlas@users.noreply.github.com>
2022-10-05 10:22:40 -0300

    Mudança no texto
    Mudança no texto e na quantidade de exibicoes.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=11;i++){
-         System.out.println("Olá seres do planeta Terra, turma 2TDSJ. Aqui Somos Todos Um! "+i);
+      for(int i=1;i<=20;i++){
+         System.out.println("Olá seres do planeta Terra, em especial a turma 2TDSJ. Aqui Somos Todos Um! "+i);
       }
    }
 }
