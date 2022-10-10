daec70551a2fb1e679cac82ae778ad319e992e0b
Oivlas <71229362+oivlas@users.noreply.github.com>
2022-10-10 08:42:22 -0300

    Ajuste do Texto para a turma 2TDSF
    Teste...

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
    public static void main (String []args){
-      for(int i=1;i<=11;i++){
-         System.out.println("Olá seres do planeta Terra, em especial a turma 2TDSA. Aqui Somos Todos Um! "+i);
+      for(int i=1;i<=20;i++){
+         System.out.println("Olá seres do planeta Terra, em especial a turma 2TDSF. Aqui Somos Todos Um! "+i);
       }
    }
 }
